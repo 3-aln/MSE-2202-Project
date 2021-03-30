5f944a988ed97c7832dc7f1d795c714423988dee
Allen Sun <sunalle184@gmail.com>
2021-03-29 22:34:51 -0400

    Ultrasonic sensor test without use of delay function

--- Testing/test_HC-SR04_ultrasonic_nano/test_HC-SR04_ultrasonic_nano.ino
+++ Testing/test_HC-SR04_ultrasonic_nano/test_HC-SR04_ultrasonic_nano.ino
@@ -6,6 +6,12 @@ const int trig_pin = 4;   // HC-SR04 ultrasonic sensor - trigger pin
 const int echo_pin = 3;   // HC-SR04 ultrasonic sensor - "echo" pin
 
 long pulse_duration, dist_cm;
+const int short_pulse = 2;  // microseconds
+const int long_pulse = short_pulse + 10; // microseconds
+
+unsigned long us_now;     // store current operation time in microseconds
+unsigned long us_prev_short_pulse;  // previous short pulse time
+unsigned long us_prev_long_pulse;   // previous long pulse time
 
 void setup() {
   Serial.begin(9600);
@@ -14,17 +20,36 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(trig_pin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trig_pin, HIGH);
-  delayMicroseconds(10);
+  us_now = micros();
 
-  pulse_duration = pulseIn(echo_pin, HIGH);
+  if (us_now - us_prev_short_pulse >= short_pulse) {
+    us_prev_short_pulse = us_now;
+    digitalWrite(trig_pin, HIGH);
+  }
 
-  dist_cm = microsecond_to_cm(pulse_duration);
+  if (us_now - us_prev_long_pulse >= long_pulse) {
+    digitalWrite(trig_pin, LOW);
 
-  Serial.print(dist_cm);
-  Serial.println(" cm");
+    pulse_duration = pulseIn(echo_pin, HIGH);
+
+    dist_cm = microsecond_to_cm(pulse_duration);
+    Serial.print(dist_cm);
+    Serial.println(" cm");
+  }
+
+
+  
+//  digitalWrite(trig_pin, LOW);
+//  delayMicroseconds(2);
+//  digitalWrite(trig_pin, HIGH);
+//  delayMicroseconds(10);
+//
+//  pulse_duration = pulseIn(echo_pin, HIGH);
+//
+//  dist_cm = microsecond_to_cm(pulse_duration);
+//
+//  Serial.print(dist_cm);
+//  Serial.println(" cm");
 }
 
 long microsecond_to_cm(long microseconds) {
